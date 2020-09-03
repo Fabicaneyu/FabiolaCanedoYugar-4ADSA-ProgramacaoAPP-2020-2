c6413fcd78933711498affe39e6e1924a71eac37
Fabi <fabicaneyu@gmail.com>
2020-09-03 00:55:02 -0300

    atualizando atividade

---
+++ atividade/app/src/main/java/com/example/fabiola_canedo_yugar_c1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.fabiola_canedo_yugar_c1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun calcular(componente: View){
+    val nomeExposto = et_nome.text
+
+        et_meta
+
+
+
+    }
+}
